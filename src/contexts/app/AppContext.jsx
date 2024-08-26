bc16e38de5d273f265d724edce032bcde98037d0
m.talebi <mahita.contact@gmail.com>
2024-08-26 09:43:23 -0400

    feat: add context

---
+++ src/contexts/app/AppContext.jsx
@@ -0,0 +1,37 @@
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useTransition,
+} from "react";
+import appReducer from "./appReducer";
+import { useTranslation } from "react-i18next";
+
+const AppContext = createContext();
+const initialState = { language: localStorage.getItem("language") || "fa" };
+
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(appReducer, initialState);
+  const { i18n } = useTranslation();
+
+  const changeLanguage = (language) => {
+    dispatch({ type: "CHANGE_LANGUAGE", payload: language });
+  };
+
+  useEffect(() => {
+    i18n.changeLanguage(state.language);
+    localStorage.setItem("language", state.language);
+  }, [state.language]);
+  return (
+    <AppContext.Provider value={{ ...state, changeLanguage }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+const useAppContext = () => {
+  return useContext(AppContext);
+};
+
+export { useAppContext, AppProvider };
