5e9cb865a41867e89c17bcd4c44294d9c2bf9a4c
m.talebi <mahita.contact@gmail.com>
2024-08-28 04:58:18 -0400

    feat: add darkMode

--- src/contexts/app/AppContext.jsx
+++ src/contexts/app/AppContext.jsx
@@ -1,15 +1,12 @@
-import {
-  createContext,
-  useContext,
-  useEffect,
-  useReducer,
-  useTransition,
-} from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import appReducer from "./appReducer";
 import { useTranslation } from "react-i18next";
 
 const AppContext = createContext();
-const initialState = { language: localStorage.getItem("language") || "fa" };
+const initialState = {
+  language: localStorage.getItem("language") || "fa",
+  theme: localStorage.getItem("theme"),
+};
 
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(appReducer, initialState);
@@ -18,13 +15,26 @@ const AppProvider = ({ children }) => {
   const changeLanguage = (language) => {
     dispatch({ type: "CHANGE_LANGUAGE", payload: language });
   };
+  const changeTheme = (theme) => {
+    dispatch({ type: "CHANGE_THEME", payload: theme });
+  };
 
   useEffect(() => {
     i18n.changeLanguage(state.language);
     localStorage.setItem("language", state.language);
+    document.body.dir = state.language === "fa" ? "rtl" : "ltr";
   }, [state.language]);
+
+  useEffect(() => {
+    localStorage.setItem("theme", state.theme);
+    if (state.theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [state.theme]);
   return (
-    <AppContext.Provider value={{ ...state, changeLanguage }}>
+    <AppContext.Provider value={{ ...state, changeLanguage, changeTheme }}>
       {children}
     </AppContext.Provider>
   );
--- src/layouts/identity-layout.jsx
+++ src/layouts/identity-layout.jsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom";
 import { ChangeLanguage } from "../components/ChangeLanguage";
+import { DarkSwitcher } from "../components/DarkSwitcher";
 
 export function IdentityLayout() {
   return (
-    <div className="flex flex-col justify-between bg-slate-100 h-screen">
-      <nav className="border-b-2 h-16">
+    <div className="flex flex-col justify-center bg-slate-100 dark:bg-slate-800 h-screen">
+      <nav className="border-b-2 dark:border-b-slate-700 w-full fixed top-0 flex px-5">
+        <DarkSwitcher />
         <ChangeLanguage />
       </nav>
-      <Outlet />
+      <main>
+        <Outlet />
+      </main>
     </div>
   );
 }
